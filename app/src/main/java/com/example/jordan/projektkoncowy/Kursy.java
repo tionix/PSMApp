a0f99557c069ea17e17e0b09e1cd67ab9971179f
Basia Lewicka <aisab.lewicka@gmail.com>
2017-05-26 18:37:27 +0200

    init

---
+++ app/src/main/java/com/example/jordan/projektkoncowy/Kursy.java
@@ -0,0 +1,13 @@
+package com.example.jordan.projektkoncowy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Kursy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kursy);
+    }
+}
---
+++ app/src/main/java/com/example/jordan/projektkoncowy/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.jordan.projektkoncowy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onValue (View view){
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.bankier.pl/surowce/notowania";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        queue.add(stringRequest);
+    }
+}
